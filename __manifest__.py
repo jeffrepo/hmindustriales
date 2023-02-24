e9e0370491abf0c588a081f14baa04e88e57a33e
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2023-02-24 00:30:20 -0600

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "HM Industriales",
+
+    'summary': """ HM Industriales """,
+
+    'description': """
+       HM Industriales
+    """,
+
+    'author': "STECHNOLOGIES",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['sale'],
+
+    'data': [
+    ],
+    'assets':{
+        'point_of_sale.assets': [
+            #'pos_technotrade/static/src/js/**/*.js',
+            #'pos_technotrade/static/src/xml/**/*.xml',
+        ],
+        'web.assets_qweb':[
+        ],
+        'web.assets_backend': [
+        ],
+    },
+    'qweb': [
+    ],
+    'license': 'LGPL-3',
+}

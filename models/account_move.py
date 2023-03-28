06e717bc017de80fd6dff092c11427fb6eddbbd4
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2023-03-27 22:40:52 -0600

    Update account_move.py

--- models/account_move.py
+++ models/account_move.py
@@ -11,3 +11,13 @@ class AccountMove(models.Model):
         for move in self:
             if move.partner_id and move.partner_id.l10n_mx_edi_usage:
                 move.l10n_mx_edi_usage = move.partner_id.l10n_mx_edi_usage
+
+
+class PosOrder(models.Model):
+    _inherit = 'pos.order'
+    
+    def buscar_stock(self, dicc_productos):
+        productos_sin_stock = []
+        logging.warning(dicc_productos)
+        productos_stock = self.env['stock.quant']
+        return productos_sin_stock
